eeadfb9b85eb2815943200b32df256e87e7c007a
Jonathan Expósito <alu0100696455@ull.edu.es>
2015-10-29 09:56:36 +0000

    Añadido test para la clase fracción

---
+++ test/tc_fraccion.rb
@@ -0,0 +1,14 @@
+require './lib/fraccion'
+require 'test/unit'
+
+class TestFraccion < Test::Unit::TestCase
+	def setup
+		@frac1 = Fraccion.new(20, 60)
+		@frac2 = Fraccion.new(15, 35)
+	end
+
+	def test_simplificar
+		assert_equal("1/3", @frac1.to_s)
+		assert_equal("3/7", @frac2.to_s)
+	end
+end
